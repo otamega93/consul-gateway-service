6ccd697fc1b27ec3cf2cddae6f27c142f3f52f6b
Manuel Páez <m.paez589@gmail.com>
2018-05-14 13:11:07 +0200

    Initial commit

---
+++ src/main/java/com/example/consulgatewayservice/ConsulGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.consulgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulGatewayServiceApplication.class, args);
+	}
+}
